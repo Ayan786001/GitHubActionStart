8440ccb97f8caeb10a8605648f36cfeb76e76938
ayan786001 <mf786001@gmail.com>
2025-02-12 14:44:05 +0100

    Initial commit

---
+++ src/main/java/kea/ayan/githubactionstart/GitHubActionStartApplication.java
@@ -0,0 +1,13 @@
+package kea.ayan.githubactionstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionStartApplication.class, args);
+	}
+
+}
